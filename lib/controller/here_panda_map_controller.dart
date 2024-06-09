4fc388293bbecb88c74d244030deae8415336a29
ptt <ptt99931.work@gmail.com>
2024-06-09 17:03:46 +0700

    [map] tracking current location

--- lib/controller/here_panda_map_controller.dart
+++ lib/controller/here_panda_map_controller.dart
@@ -6,18 +6,15 @@ import 'package:here_sdk/core.dart';
 import 'package:here_sdk/core.engine.dart';
 import 'package:here_sdk/core.errors.dart';
 import 'package:here_sdk/mapview.dart';
+import 'package:location_platform_interface/location_platform_interface.dart';
 import 'package:panda_map/assets/assets.dart';
 import 'package:panda_map/core/controllers/panda_map_controller.dart';
 import 'package:panda_map/core/models/map_lat_lng.dart';
 import 'package:panda_map/core/models/map_location.dart';
-import 'package:panda_map/core/services/map_service.dart';
 import 'package:panda_map/utils/asset_utils.dart';
 
 class HerePandaMapController extends PandaMapController {
-  HerePandaMapController({MapService? service})
-      : _mapService = service ?? MapService();
-
-  late LocationIndicator _locationIndicator;
+  HerePandaMapController();
 
   // final Set<Marker> markers = <Marker>{};
   // final Set<Circle> circles = <Circle>{};
@@ -28,7 +25,36 @@ class HerePandaMapController extends PandaMapController {
   MapMarker? _currentLocationMarker;
 
   final int _currentMapTypeIndex = 1;
-  final MapService _mapService;
+  LocationIndicator? _locationIndicator;
+
+  @override
+  Future<void> initMap(covariant HerePandaMapOptions options) async {
+    SdkContext.init(IsolateOrigin.main);
+    String accessKeyId = options.mapAPIKeyId;
+    String accessKeySecret = options.mapAPIKey;
+    SDKOptions sdkOptions =
+        SDKOptions.withAccessKeySecret(accessKeyId, accessKeySecret);
+    try {
+      await SDKNativeEngine.makeSharedInstance(sdkOptions);
+    } on InstantiationException {
+      throw Exception("Failed to initialize the HERE SDK.");
+    }
+  }
+
+  @override
+  void dispose() {
+    SDKNativeEngine.sharedInstance?.dispose();
+    SdkContext.release();
+    super.dispose();
+  }
+
+  @override
+  void onLocationChanged(LocationData event) {
+    focusMapTo(MapLocation.fromLocationData(event));
+    _locationIndicator?.updateLocation(Location.withCoordinates(
+      GeoCoordinates(event.latitude!, event.longitude!),
+    ));
+  }
 
   @override
   void addMarker(MapLatLng latlng) {
@@ -62,20 +88,16 @@ class HerePandaMapController extends PandaMapController {
   }
 
   @override
-  void dispose() {
-    SDKNativeEngine.sharedInstance?.dispose();
-    SdkContext.release();
-    super.dispose();
-  }
-
-  @override
   Future<void> focusCurrentLocation({bool animate = true}) async {
-    MapLocation? location = await _mapService.getCurrentLocation();
+    MapLocation? location = await mapService.getCurrentLocation();
     if (location != null) {
       focusMapTo(location);
-      _locationIndicator.updateLocation(
-        Location.withCoordinates(location.toHereMapCoordinate()),
-      );
+      // Run by contorl to mark sure called after mapInint
+      control((_) async {
+        _locationIndicator?.updateLocation(
+          Location.withCoordinates(location.toHereMapCoordinate()),
+        );
+      });
     } else {
       log('Google map: FInd location failed');
     }
@@ -83,7 +105,7 @@ class HerePandaMapController extends PandaMapController {
 
   @override
   Future<void> focusMapTo(MapLocation location, {bool animate = true}) async {
-    return control((HereMapController controller) {
+    return control((HereMapController controller) async {
       MapCameraAnimation amim = MapCameraAnimationFactory.flyTo(
         GeoCoordinatesUpdate(location.lat, location.long),
         1,
@@ -93,23 +115,12 @@ class HerePandaMapController extends PandaMapController {
     });
   }
 
-  @override
-  Future<void> init(covariant HerePandaMapOptions options) async {
-    SdkContext.init(IsolateOrigin.main);
-    String accessKeyId = options.mapAPIKeyId;
-    String accessKeySecret = options.mapAPIKey;
-    SDKOptions sdkOptions =
-        SDKOptions.withAccessKeySecret(accessKeyId, accessKeySecret);
-    try {
-      await SDKNativeEngine.makeSharedInstance(sdkOptions);
-    } on InstantiationException {
-      throw Exception("Failed to initialize the HERE SDK.");
-    }
-  }
+  bool get isMapInitilized => _controllerCompleter.isCompleted;
 
-  Future<HereMapController> get controllerFuture => _controllerCompleter.future;
+  late Future<HereMapController> controllerFuture = _controllerCompleter.future;
 
   int get currentMapTypeIndex => _currentMapTypeIndex;
+  HereMapController? _controller;
 
   void onMapCreated(HereMapController controller) {
     load(() async {
@@ -118,6 +129,13 @@ class HerePandaMapController extends PandaMapController {
         _controllerCompleter = Completer();
       }
       _controllerCompleter.complete(controller);
+      _controller = controller;
+      // Setup current location indicator
+      _locationIndicator?.disable();
+      _locationIndicator = LocationIndicator()
+        ..locationIndicatorStyle = LocationIndicatorIndicatorStyle.pedestrian;
+      _locationIndicator?.enable(controller);
+      await focusCurrentLocation(animate: false);
 
       // Load map
       Completer<bool> loadComplete = Completer<bool>();
@@ -128,25 +146,19 @@ class HerePandaMapController extends PandaMapController {
             log('Map scene not loaded. MapError: ${error.toString()}');
             return;
           }
-
-          // Setup current location indicator
-          _locationIndicator = LocationIndicator()
-            ..locationIndicatorStyle =
-                LocationIndicatorIndicatorStyle.pedestrian;
-          _locationIndicator.enable(controller);
-          await focusCurrentLocation(animate: false);
-
           loadComplete.complete(true);
         },
       );
 
-      await loadComplete.future;
+      // Tracking current location
     });
   }
 
-  Future<void> control(Function(HereMapController controller) action) async {
-    HereMapController controller = await controllerFuture;
-    action(controller);
+  Future<void> control(
+    Future<void> Function(HereMapController controller) action,
+  ) async {
+    HereMapController controller = _controller ?? await controllerFuture;
+    await action(controller);
   }
 
   // Add marker
