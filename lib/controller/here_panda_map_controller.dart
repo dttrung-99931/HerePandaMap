eb4c9da30b4d4d685435d04b6a6d2219ddca1d2a
ptt <ptt99931.work@gmail.com>
2024-06-09 23:03:35 +0700

    [map] done focus & track current location

--- lib/controller/here_panda_map_controller.dart
+++ lib/controller/here_panda_map_controller.dart
@@ -9,6 +9,7 @@ import 'package:here_sdk/mapview.dart';
 import 'package:location_platform_interface/location_platform_interface.dart';
 import 'package:panda_map/assets/assets.dart';
 import 'package:panda_map/core/controllers/panda_map_controller.dart';
+import 'package:panda_map/core/models/map_current_location.dart';
 import 'package:panda_map/core/models/map_lat_lng.dart';
 import 'package:panda_map/core/models/map_location.dart';
 import 'package:panda_map/utils/asset_utils.dart';
@@ -16,6 +17,9 @@ import 'package:panda_map/utils/asset_utils.dart';
 class HerePandaMapController extends PandaMapController {
   HerePandaMapController();
 
+  late Future<HereMapController> controllerFuture = _controllerCompleter.future;
+
+  HereMapController? _controller;
   // final Set<Marker> markers = <Marker>{};
   // final Set<Circle> circles = <Circle>{};
   Completer<HereMapController> _controllerCompleter = Completer();
@@ -41,19 +45,37 @@ class HerePandaMapController extends PandaMapController {
     }
   }
 
-  @override
-  void dispose() {
-    SDKNativeEngine.sharedInstance?.dispose();
-    SdkContext.release();
-    super.dispose();
-  }
+  void onMapCreated(HereMapController controller) {
+    load(() async {
+      // onMapCreated may be called many times by MapScreen due to back to home screen
+      if (_controllerCompleter.isCompleted) {
+        _controllerCompleter = Completer();
+      }
+      _controllerCompleter.complete(controller);
+      _controller = controller;
 
-  @override
-  void onLocationChanged(LocationData event) {
-    focusMapTo(MapLocation.fromLocationData(event));
-    _locationIndicator?.updateLocation(Location.withCoordinates(
-      GeoCoordinates(event.latitude!, event.longitude!),
-    ));
+      // Setup current location indicator
+      _locationIndicator?.disable();
+      _locationIndicator = LocationIndicator()
+        ..locationIndicatorStyle = LocationIndicatorIndicatorStyle.pedestrian;
+      _locationIndicator?.enable(controller);
+      await focusCurrentLocation(animate: false);
+
+      // Load map
+      Completer<bool> loadComplete = Completer<bool>();
+      controller.mapScene.loadSceneForMapScheme(
+        MapScheme.normalDay,
+        (MapError? error) async {
+          if (error != null) {
+            log('Map scene not loaded. MapError: ${error.toString()}');
+            return;
+          }
+          loadComplete.complete(true);
+        },
+      );
+
+      // Tracking current location
+    });
   }
 
   @override
@@ -88,23 +110,31 @@ class HerePandaMapController extends PandaMapController {
   }
 
   @override
-  Future<void> focusCurrentLocation({bool animate = true}) async {
-    MapLocation? location = await mapService.getCurrentLocation();
-    if (location != null) {
-      focusMapTo(location);
-      // Run by contorl to mark sure called after mapInint
-      control((_) async {
-        _locationIndicator?.updateLocation(
-          Location.withCoordinates(location.toHereMapCoordinate()),
-        );
-      });
+  void dispose() {
+    SDKNativeEngine.sharedInstance?.dispose();
+    SdkContext.release();
+    super.dispose();
+  }
+
+  @override
+  Future<void> focusCurrentLocation({
+    MapCurrentLocation? currentLocation,
+    bool animate = true,
+  }) async {
+    currentLocation ??= await mapService.getCurrentLocation();
+    if (currentLocation != null) {
+      focusLocation(currentLocation);
+      updateCurLocationIndicator(currentLocation);
     } else {
       log('Google map: FInd location failed');
     }
   }
 
   @override
-  Future<void> focusMapTo(MapLocation location, {bool animate = true}) async {
+  Future<void> focusLocation(
+    MapLocation location, {
+    bool animate = true,
+  }) async {
     return control((HereMapController controller) async {
       MapCameraAnimation amim = MapCameraAnimationFactory.flyTo(
         GeoCoordinatesUpdate(location.lat, location.long),
@@ -115,44 +145,23 @@ class HerePandaMapController extends PandaMapController {
     });
   }
 
-  bool get isMapInitilized => _controllerCompleter.isCompleted;
+  @override
+  void onLocationChanged(LocationData event) {
+    updateCurLocationIndicator(MapCurrentLocation.fromLocationData(event));
+  }
 
-  late Future<HereMapController> controllerFuture = _controllerCompleter.future;
-
-  int get currentMapTypeIndex => _currentMapTypeIndex;
-  HereMapController? _controller;
-
-  void onMapCreated(HereMapController controller) {
-    load(() async {
-      // onMapCreated may be called many times by MapScreen due to back to home screen
-      if (_controllerCompleter.isCompleted) {
-        _controllerCompleter = Completer();
-      }
-      _controllerCompleter.complete(controller);
-      _controller = controller;
-      // Setup current location indicator
-      _locationIndicator?.disable();
-      _locationIndicator = LocationIndicator()
-        ..locationIndicatorStyle = LocationIndicatorIndicatorStyle.pedestrian;
-      _locationIndicator?.enable(controller);
-      await focusCurrentLocation(animate: false);
-
-      // Load map
-      Completer<bool> loadComplete = Completer<bool>();
-      controller.mapScene.loadSceneForMapScheme(
-        MapScheme.normalDay,
-        (MapError? error) async {
-          if (error != null) {
-            log('Map scene not loaded. MapError: ${error.toString()}');
-            return;
-          }
-          loadComplete.complete(true);
-        },
+  void updateCurLocationIndicator(MapCurrentLocation? currentLocation) {
+    // Run in control() to mark sure called after mapInint
+    control((_) async {
+      _locationIndicator?.updateLocation(
+        Location.withCoordinates(currentLocation!.toHereMapCoordinate()),
       );
-
-      // Tracking current location
     });
   }
+
+  bool get isMapInitilized => _controllerCompleter.isCompleted;
+
+  int get currentMapTypeIndex => _currentMapTypeIndex;
 
   Future<void> control(
     Future<void> Function(HereMapController controller) action,
